20e660e5369e637bbe0181b0927b0879b89fcc52
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-23 08:45:21 -0300

    Create MeuComponente.vue

---
+++ src/components/MeuComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Esse é um componente personalizado!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeuComponente'
+}
+</script>
+
+<style scoped>
+p {
+  color: #42b983;
+  font-weight: bold;
+}
+</style>
