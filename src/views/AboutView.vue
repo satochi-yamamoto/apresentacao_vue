2120d01f41cd373050b04d7cb68e488541cfed54
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-23 08:39:18 -0300

    Create AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>Sobre este projeto</h1>
+    <p>Este é um site de teste criado para aprender Vue.js e suas funcionalidades.</p>
+    <p>Você pode personalizar este projeto adicionando novas páginas, componentes e funcionalidades.</p>
+    
+    <h2>Tecnologias usadas</h2>
+    <ul>
+      <li>Vue.js 3</li>
+      <li>Vue Router</li>
+      <li>Pinia</li>
+      <li>Vite</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: left;
+}
+
+ul {
+  list-style-type: square;
+  padding-left: 20px;
+}
+</style>
