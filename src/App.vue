ea977e466a83788649271e9bec6c86bcb84a663c
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-23 10:02:25 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alexandre Satochi Yamamoto - Especialista TI, Desenvolvimento de Software e Gestão</title>
+    <meta name="description" content="Profissional de TI e gestão com experiência em Desenvolvimento de Software e Infraestrutura">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
 <template>
   <div class="container">
      
