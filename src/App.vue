381fbde6f2c66bce1a9d1d3ef4209629238c5cde
Alexandre Yamamoto <alextoyama@hotmail.com>
2025-06-23 21:59:14 -0300

    Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
             <li><a href="#about">Sobre</a></li>
             <li><a href="#experience">Experiência</a></li>
             <li><a href="#education">Formação</a></li>
+            <li><a href="#volunteer">Voluntario</a></li>
             <li><a href="#skills">Habilidades</a></li>
             <li><a href="#contact">Contato</a></li>
           </ul>
